4dcfdbe6f3f0d3683b3aaea7062621c30fb4ac27
caginbaris <31400655+caginbaris@users.noreply.github.com>
2017-11-18 11:38:55 +0200

    GPIO cont.

--- Inc/ios.h
+++ Inc/ios.h
@@ -7,16 +7,67 @@
 
 struct digitalInputs{
 
-	uint32_t start_stop:1;
-	uint32_t Q1_cb_pos:1;
-	uint32_t Q2_cb_pos:1;
+	uint32_t start_stop:1; 					// DI-1
+	uint32_t Q1_cb_pos:1;						// DI-2
+	uint32_t Q2_cb_pos:1;						// DI-3
+	uint32_t Q3_cb_pos:1;						// DI-4	
+	uint32_t EAF1_cb_pos:1;					// DI-5	
+	uint32_t EAF3_cb_pos:1;					// DI-6
+	uint32_t EAF2_cb_pos:1;					// DI-7
+	uint32_t LF1_cb_pos:1;					// DI-8
+	uint32_t LF2_cb_pos:1;					// DI-9
+	uint32_t SVC_TRIP:1;						// DI-10
+	uint32_t LF3_cb_pos:1;					// DI-11
+	uint32_t Q1_open_ctb:1;					// DI-12
+	uint32_t Q1_trip_ctb:1;					// DI-13
+	uint32_t Q2_open_ctb:1;					// DI-14
+	uint32_t Q2_trip_ctb:1;					// DI-15
+	uint32_t Q3_open_ctb:1;					// DI-16
+	uint32_t Q3_trip_ctb:1;					// DI-17
+	uint32_t Furnace3_SWT:1;				// DI-18
+	uint32_t _52:1;									// DI-19
+	uint32_t _53:1;									// DI-20
+	uint32_t _54:1;									// DI-21
+	uint32_t _55:1;									// DI-22
+	uint32_t _56:1;									// DI-23
+	uint32_t COLD_TEST:1;						// DI-24
 	
-
+	uint32_t PLB_INT_DO_SPARE2:1;		// DI-25
+	uint32_t PLB_INT_DO_SPARE1:1;		// DI-26
+	uint32_t PLB_INT_DO_SPARE4:1;		// DI-27
+	uint32_t PLB_INT_DO_SPARE3:1;		// DI-28
+	uint32_t PLB_INT_DO_SPARE6:1;		// DI-26
+	uint32_t PLB_INT_DO_SPARE5:1;		// DI-25
 
 };
 
 
+extern struct digitalInputs DI;
 
+
+union digitalInputUnion  {
+
+	struct digitalInputs DI;
+	uint32_t all;
+
+
+};
+
+extern union digitalInputUnion uDI;
+
+
+
+struct digitalOutputs{
+	
+	uint8_t CTB1_READY:1;		// DO-1
+	uint8_t CTB1_RUN:1;			// DO-2
+	uint8_t CTB1_IBF:1;			// DO-2
+	uint8_t CTB1_SPARE:1;		// DO-2
+
+};
+
+
+extern struct digitalOutputs DO;
 
 
 #endif
--- MDK-ARM/ios.c
+++ MDK-ARM/ios.c
@@ -7,9 +7,32 @@
 
 
 void ios(void){
+	
+	
+	DI.start_stop		=HAL_GPIO_ReadPin(START_STOP_DI_1_GPIO_Port,START_STOP_DI_1_Pin);
+	DI.Q1_cb_pos 		=HAL_GPIO_ReadPin(Q1_CB_POS_DI_2_GPIO_Port,Q1_CB_POS_DI_2_Pin);
+	DI.Q2_cb_pos 		=HAL_GPIO_ReadPin(Q2_CB_POS_DI_3_GPIO_Port,Q2_CB_POS_DI_3_Pin);
+	DI.Q3_cb_pos 		=HAL_GPIO_ReadPin(Q3_CB_POS_DI_4_GPIO_Port,Q3_CB_POS_DI_4_Pin);
+	DI.EAF1_cb_pos 	=HAL_GPIO_ReadPin(EAF1_CB_POS_DI_5_GPIO_Port,EAF1_CB_POS_DI_5_Pin);
+	DI.EAF2_cb_pos 	=HAL_GPIO_ReadPin(EAF2_CB_POS_DI_7_GPIO_Port,EAF2_CB_POS_DI_7_Pin);
+	DI.EAF3_cb_pos 	=HAL_GPIO_ReadPin(EAF3_CB_POS_DI_6_GPIO_Port,EAF3_CB_POS_DI_6_Pin );
+	DI.LF1_cb_pos		=HAL_GPIO_ReadPin(LF1_CB_POS_DI8_GPIO_Port,LF1_CB_POS_DI8_Pin);
+	DI.LF2_cb_pos		=HAL_GPIO_ReadPin(LF2_CB_POS_DI_9_GPIO_Port,LF2_CB_POS_DI_9_Pin);
+	DI.LF3_cb_pos		=HAL_GPIO_ReadPin(LF3_CB_POS_DI_11_GPIO_Port,LF3_CB_POS_DI_11_Pin);
+	DI.SVC_TRIP			=HAL_GPIO_ReadPin(SVC_TRIP_DI_10_GPIO_Port,SVC_TRIP_DI_10_Pin);
+	DI.Q1_open_ctb 	=HAL_GPIO_ReadPin(Q1_CB_OPEN_DI_12_GPIO_Port,Q1_CB_OPEN_DI_12_Pin);
+	DI.Q2_open_ctb 	=HAL_GPIO_ReadPin(Q2_CB_OPEN_DI_14_GPIO_Port,Q2_CB_OPEN_DI_14_Pin);
+	DI.Q3_open_ctb 	=HAL_GPIO_ReadPin(Q3_CB_OPEN_DI_16_GPIO_Port,Q3_CB_OPEN_DI_16_Pin);
+	DI.Q1_trip_ctb	=HAL_GPIO_ReadPin(Q1_CB_TRIP_DI_13_GPIO_Port,Q1_CB_TRIP_DI_13_Pin);
+	DI.Q2_trip_ctb	=HAL_GPIO_ReadPin(Q2_CB_TRIP_DI_15_GPIO_Port,Q2_CB_TRIP_DI_15_Pin);
+	DI.Q3_trip_ctb	=HAL_GPIO_ReadPin(Q3_CB_TRIP_DI_17_GPIO_Port,Q3_CB_TRIP_DI_17_Pin);
+	DI._52					=HAL_GPIO_ReadPin(DI_52_DI_19_GPIO_Port,DI_52_DI_19_Pin);
+	DI._53					=HAL_GPIO_ReadPin(DI_53_DI_20_GPIO_Port,DI_53_DI_20_Pin);
+	DI._54					=HAL_GPIO_ReadPin(DI_54_DI_21_GPIO_Port,DI_54_DI_21_Pin);
+	
+	
+	
 
-
-
-
+	
 }
 
